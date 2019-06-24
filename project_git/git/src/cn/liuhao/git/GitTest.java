624a0618b2d3f0b7428e05ceffec725dfaed1f9a
Mrs Liu <891836418@qq.com>
2019-06-24 18:25:42 +0800

    把本地工程添加到git

---
+++ project_git/git/src/cn/liuhao/git/GitTest.java
@@ -0,0 +1,14 @@
+package cn.liuhao.git;
+
+/**
+ * @Author: mrs Liu
+ * @Date: 2019/6/24 18:23
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+    }
+}
